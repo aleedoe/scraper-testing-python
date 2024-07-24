343eb76d5ecc53e3e10fa5bda19da6d1a5343408
alie <mushyafaali@gmail.com>
2024-07-24 12:56:50 +0700

    update onepage-tokped.py

--- onepage-tokped.py
+++ onepage-tokped.py
@@ -11,83 +11,52 @@ import time
 def fetchingProducts(wait, driver):
     page = 1
     
+    get_url = driver.current_url
+    print("The current url is:"+str(get_url))
+    driver.get(str(get_url) + '?perpage=10')
+        
+    # Klik elemen setelah mengganti href
+    # per_page_element = wait.until(
+    #     EC.element_to_be_clickable((By.CSS_SELECTOR, 'span[class="css-10nze3y"]'))
+    # )
+    # per_page_element.click()
+    
     while True:
         print(f"============= produk halaman: {page} ===============")
-        
-        # Scroll down
-        time.sleep(4)  # Jika Anda tetap ingin memberikan waktu tidur, ini bisa dipertahankan
-        driver.execute_script("window.scrollBy(0, 800);")
         try:
-            # Tunggu sampai elemen terakhir muncul
-            wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'css-1sn1xa2')))
-        except Exception as e:
-            print(f"Error waiting after scroll: {e}")
-            break
-        
-        
-        try:
-            # Ambil nama produk
-            # products = wait.until(EC.visibility_of_element_located((By.CLASS_NAME, 'css-1sn1xa2')))
-            products = wait.until(EC.visibility_of_element_located((By.CLASS_NAME, 'css-tjjb18')))
-
-            # Find all product elements within the container
-            produk2 = products.find_elements(By.CLASS_NAME, 'css-1sn1xa2')
-
-            # Print each product element
-            for i, produk in enumerate(produk2, start=1):
-                products_container = wait.until(EC.visibility_of_element_located((By.CLASS_NAME, 'css-tjjb18')))
-                produk2 = products_container.find_elements(By.CLASS_NAME, 'css-1sn1xa2')
-                produk = produk2[i]
-                print(f"Product {i}:")
-                print(produk.text)
-                print("-" * 20)
-                produk.click()
-                driver.back()
-            break
+            products = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'css-1sn1xa2')))
+            print(f"jumlah data: {len(products)}")
             
-            for index, product in enumerate(products):
+            for index in range(len(products)):
                 try:
-                    # Ambil ulang produk berdasarkan indeks
-                    driver.execute_script("window.scrollBy(0, 800);")
-                    products = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'css-1sn1xa2')))
+                    # Refresh the list of products to avoid stale element reference
+                    container = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div[class="css-tjjb18"]')))
+                    result_container = container.find_elements(By.CSS_SELECTOR, 'div[class="css-1sn1xa2"]')
                     
-                    try:
-                        # Tunggu sampai elemen terakhir muncul setelah scroll
-                        wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'css-1sn1xa2')))
-                    except Exception as e:
-                        print(f"Error waiting after scroll: {e}")
-                        break
+                    product = result_container[index]
                     
-                    product = products[index]
-                    
-                    # Klik produk
+                    print(f"jumlah data saat looping: {len(products)}")
+                    driver.execute_script("arguments[0].scrollIntoView(true);", product)
                     wait.until(EC.element_to_be_clickable(product)).click()
-                    title = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'h1[data-testid="lblPDPDetailProductName"]')))
+                    
+                    title = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="pdp_comp-product_content"]/div/div[1]/h1')))
                     print(f"nama ke-{index + 1}: {title.text}")
                     
-                    # Kembali ke halaman sebelumnya
                     driver.back()
                     
-                    # Tunggu sampai halaman kembali sepenuhnya
-                    wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'css-1sn1xa2')))
-                    
-                    if index == enumerate(products):
-                        try:
-                            # Find the pagination element again before clicking
-                            paginations = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'a[data-testid="btnShopProductPageNext"]')))
-                            paginations.click()
-                            page += 1
-                        except Exception as e:
-                            print(f"Error clicking pagination: {e}")
-                            break
-                        
+                    if index == len(products) - 1:
+                        paginations = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'a[data-testid="btnShopProductPageNext"]')))
+                        driver.execute_script("arguments[0].click();", paginations)
+                        wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'div[class="css-1sn1xa2"]')))
+                        page += 1
                 except Exception as e:
-                    print(f"Error mendapatkan title: {e}")
-                    break
-                
+                    print(f"Kesalahan saat memproses produk ke-{index + 1}: {e}")
+                    continue
         except Exception as e:
-            print(f"Error getting products: {e}")
-            break    
+            print(e)
+            print("===== ambil produk selesai =======")
+            return False
+
 
 
 def startScraping(chrome_driver_path, keyword_search):
@@ -95,35 +64,36 @@ def startScraping(chrome_driver_path, keyword_search):
     service = Service(chrome_driver_path)
     driver = webdriver.Chrome(service=service, options=chrome_options)
     driver.maximize_window()
-    driver.implicitly_wait(30)
+    driver.implicitly_wait(20)
     try:
         driver.get("https://www.tokopedia.com")
-        wait = WebDriverWait(driver, 20)  # Meningkatkan waktu tunggu
-
-        # Cari kotak pencarian
+        wait = WebDriverWait(driver, 20)
         search_box = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'exxxdg63')))
         search_box.send_keys(keyword_search)
         search_box.send_keys(Keys.ENTER)
-
-        # Cari tombol toko
-        search_toko = wait.until(EC.element_to_be_clickable((By.XPATH, "//*[@id='zeus-root']/div/div[2]/div/div[2]/div[1]/div/button[2]")))
+        search_toko = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'button[data-testid="btnSRPShopTab"]')))
         search_toko.click()
-
-        # Klik toko
-        klik_toko = wait.until(EC.element_to_be_clickable((By.XPATH, "//*[@id='zeus-root']/div/div[2]/div/div[2]/div[2]/div[2]/div/div/a")))
-        klik_toko.click()
-
-        # Klik produk
+        klik_toko = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'div[data-testid="shop-card"]')))
+        for result in klik_toko:
+            try:
+                toko_name_element = result.find_element(By.CSS_SELECTOR, 'span[data-testid="spnSRPShopName"]')
+                toko_name = toko_name_element.text
+                if keyword_search in toko_name:
+                    toko_header = result.find_element(By.CSS_SELECTOR, 'a[data-testid="shop-card-header"]')
+                    toko_header.click()
+                    break
+            except Exception as e:
+                print(f"Terjadi kesalahan: {e}")
+        else:
+            print("Tidak ditemukan toko yang sesuai")
         klik_produk = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'button[data-testid="Produk"]')))
         klik_produk.click()
-        
         fetchingProducts(wait, driver)
-
     finally:
         driver.quit()
 
 
-chrome_driver_path = "D:\Programs\chromedriver-win64\chromedriver-win64\chromedriver.exe"
+chrome_driver_path = "D:\\Programs\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe"
 keyword_search = "Gateway Indonesia Comp"
 
 startScraping(chrome_driver_path, keyword_search)
